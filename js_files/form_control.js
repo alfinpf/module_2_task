80c4c5af55bc86e53f246282aef2c5de410060eb
alfinpf <alfinpf@gmail.com>
2023-11-21 02:50:00 +0530

    js validations added

--- js_files/form_control.js
+++ js_files/form_control.js
@@ -7,10 +7,52 @@ function formSubmit(e){
     let userPassword = document.getElementById("userPassword").value;
     let userAddress = document.getElementById("userAddress").value;
 
+ 
+    if (userFullName == null || userFullName == '' || userFullName.length < 3 || userFullName.length > 35){
+
+        let userFullNameError = document.getElementById("userFullNameError");
+        userFullNameError.innerHTML = "Enter Your Full Name to continue (3 to 35 characters are allowed)";
+        userFullNameError.style.display = 'block';
+        return false;    
+     }else{
+        userFullNameError.innerHTML = "";
+        userFullNameError.style.display = 'none';
+     }
+
+    if (userEmail == null || userEmail == ''){
+        let userEmailError = document.getElementById("userEmailError");
+        userEmailError.innerHTML = "Please enter your email-ID to continue";
+        userEmailError.style.display = 'block';
+        return false;
+    }else{
+        userEmailError.innerHTML = "";
+        userEmailError.style.display = 'none';
+    }
+
+    if (userPassword == null || userPassword == '' || userPassword.length < 8 || userPassword.length > 16 ){
+        let userPasswordError = document.getElementById("userPasswordError");
+        userPasswordError.innerHTML = "Please enter your password (8 to 16 characters)";
+        userPasswordError.style.display = 'block';
+        return false;
+    }else{
+        userPasswordError.innerHTML = "";
+        userPasswordError.style.display = 'none';
+    }
+
+    if (userAddress == null || userAddress == '' || userAddress.length < 15 || userAddress.length > 50 ){
+        let userAddressError = document.getElementById("userAddressError");
+        userAddressError.innerHTML = "Please enter your full address (15-50 characters are allowed)";
+        userAddressError.style.display = 'block';
+        return false;
+    }else{
+        userAddressError.innerHTML = "";
+        userAddressError.style.display = 'none';  
+    }
+
     console.log("Full Name:",userFullName);
     console.log("Email ID:",userEmail);
     console.log("Password:",userPassword);
     console.log("Address:",userAddress);
-    alert("Account Created Successfully !")
+    if(!alert("Account Created Successfully !")){window.location.reload();}
 }
 
--- js_files/form_control2.js
+++ js_files/form_control2.js
@@ -5,8 +5,27 @@ function formSubmit(f){
     let userEmail = document.getElementById("userEmail").value;
     let userPassword = document.getElementById("userPassword").value;
 
+    if (userEmail == null || userEmail == ''){
+        let userEmailError = document.getElementById("userEmailError");
+        userEmailError.innerHTML = "Please enter your email-ID to continue";
+        userEmailError.style.display = 'block';
+        return false;
+    }else{
+        userEmailError.innerHTML = "";
+        userEmailError.style.display = 'none';
+    }
+    
+    if (userPassword == null || userPassword == '' || userPassword.length < 8 || userPassword.length > 16 ){
+        let userPasswordError = document.getElementById("userPasswordError");
+        userPasswordError.innerHTML = "Please enter your password (8 to 16 characters)";
+        userPasswordError.style.display = 'block';
+        return false;
+    }else{
+        userPasswordError.innerHTML = "";
+        userPasswordError.style.display = 'none';
+    }
     console.log("Email ID:",userEmail);
     console.log("Password:",userPassword);
-    alert("Login Successful !")
+    if(!alert("Login Successful !")){window.location.reload();}
 }
 
